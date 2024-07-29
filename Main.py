de27e42b08ec9578d0cbd39fd52783aa8afbdf30
rohit12043 <rohitsinha12043@gmail.com>
2024-07-29 10:30:50 +0530

    Add try-catch in Menu

--- Main.py
+++ Main.py
@@ -9,19 +9,24 @@ while True:
         3. File Encryption
         4. Exit""")
 
-    choice = int(input("Enter your choice : "))
+    try:
+        choice = int(input("Enter your choice: "))
 
-    if choice == 1:
-        Steganography.menu()
+        if choice == 1:
+            Steganography.menu()
 
-    elif choice == 2:
-        lsb.menu()
+        elif choice == 2:
+            lsb.menu()
 
-    elif choice == 3:
-        encrypt.menu()
-    
-    elif choice == 4:
-        exit()
+        elif choice == 3:
+            encrypt.menu()
 
-    else:
-        print("Please select from given options")
+        elif choice == 4:
+            print("Exiting...")
+            break
+
+    except ValueError:
+        print("Invalid input. Please enter a number between 1 and 4.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+ 
